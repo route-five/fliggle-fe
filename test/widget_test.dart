d4cce528ae124098b9a19db2f7cebeda85f2478e
Cyan <65375075+hoony6134@users.noreply.github.com>
2025-07-16 00:45:18 +0900

    refactor: `snake_case` style and PR template update (#43)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fliggle/main.dart';
+// import 'package:fliggle/main.dart';
 import 'package:fliggle/app/app.dart';
 
 void main() {
