77007b33ef57debfb3096c086349ced2c1f76d2b
Cyan <hoony6134@gmail.com>
2025-07-17 00:31:41 +0900

    fix: text field style following focus state

--- lib/app/view/core/design/fliggle_text_styles.dart
+++ lib/app/view/core/design/fliggle_text_styles.dart
@@ -17,10 +17,12 @@ class FliggleTextStyles {
       );
   
   // (Custom)TextField
-  static TextStyle textFieldLabel(BuildContext context) => TextStyle(
+  static TextStyle textFieldLabel(BuildContext context, {bool isFocused = false}) => TextStyle(
         fontSize: 14, // 12 너무 작아서 14로 변경했음
         fontWeight: FontWeight.w400,
-        color: FliggleColors.of(context).disabled,
+        color: isFocused
+            ? FliggleColors.of(context).primary
+            : FliggleColors.of(context).disabled,
       );
   static TextStyle textFieldHint(BuildContext context) => TextStyle(
         fontSize: 14,
