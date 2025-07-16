4180f5be26ea61130d938b014b1427dbfaa69e93
Yeeun050211 <yetwin05@gm.gist.ac.kr>
2025-07-16 16:47:16 +0900

    fix: sync custom text field

--- lib/app/view/core/components/custom_text_field.dart
+++ lib/app/view/core/components/custom_text_field.dart
@@ -1,52 +1,50 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  final String labelText;
-  final String hintText;
+  final String label;
+  final String hint;
 
-  const CustomTextField({
-    super.key,
-    required this.labelText,
-    required this.hintText,
-  });
+  const CustomTextField({super.key, required this.label, required this.hint});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 76, // 전체 높이: 라벨 + 필드 포함
-      child: TextFormField(
-        style: const TextStyle(
-          fontSize: 14,
-          height: 1.2,
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return TextField(
+      style: TextStyle(
+        fontFamily: 'Pretendard',
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: colorScheme.onSurface,
+      ),
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(12),
+        labelText: label,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        hintText: hint,
+        hintStyle: TextStyle(
           fontFamily: 'Pretendard',
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: colorScheme.onSurface.withValues(alpha: 0.4),
         ),
-        decoration: InputDecoration(
-          labelText: labelText,
-          hintText: hintText,
-          floatingLabelBehavior: FloatingLabelBehavior.auto,
-          labelStyle: const TextStyle(
-            fontSize: 14,
-            fontFamily: 'Pretendard',
-            fontWeight: FontWeight.w500,
-            color: Colors.black,
-          ),
-          hintStyle: const TextStyle(
-            color: Colors.grey,
-            fontFamily: 'Pretendard',
-            fontSize: 14,
-          ),
-          contentPadding: const EdgeInsets.symmetric(
-            vertical: 18,
-            horizontal: 16,
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Color(0xFFB6B6B6), width: 1),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Color(0xFF6C98FF), width: 1.5),
-            borderRadius: BorderRadius.circular(12),
-          ),
+        labelStyle: TextStyle(
+          fontFamily: 'Pretendard',
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+          color: colorScheme.onSurface,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(color: colorScheme.outline, width: 1),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(color: colorScheme.outline, width: 1),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(color: colorScheme.primary, width: 1),
         ),
       ),
     );
