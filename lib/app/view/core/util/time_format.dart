a912c8ff32eae3ede9ab34bca563d5e61f136244
Hyunseung Ryu <rhseungg@gmail.com>
2025-07-18 13:22:32 +0900

    refactor: time format using extension

--- lib/app/view/core/util/time_format.dart
+++ lib/app/view/core/util/time_format.dart
@@ -1,21 +1,23 @@
-String timeFormat(DateTime time) {
-  final DateTime now = DateTime.now();
-  final Duration difference = now.difference(time);
+extension DateTimeFormat on DateTime {
+  String toRelativeTimeString() {
+    final DateTime now = DateTime.now();
+    final Duration difference = now.difference(this);
 
-  if (difference.inSeconds < 60) {
-    return '${difference.inSeconds}초 전';
-  } else if (difference.inMinutes < 60) {
-    return '${difference.inMinutes}분 전';
-  } else if (difference.inHours < 24) {
-    return '${difference.inHours}시간 전';
-  } else if (difference.inDays < 7) {
-    return '${difference.inDays}일 전';
-  } else if (difference.inDays < 30) {
-    final weeks = (difference.inDays / 7).floor();
-    return '$weeks주 전';
-  } else if (now.year == time.year) {
-    return '${time.month}월 ${time.day}일';
-  } else {
-    return '${time.year}년 ${time.month}월 ${time.day}일';
+    if (difference.inSeconds < 60) {
+      return '${difference.inSeconds}초 전';
+    } else if (difference.inMinutes < 60) {
+      return '${difference.inMinutes}분 전';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours}시간 전';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays}일 전';
+    } else if (difference.inDays < 30) {
+      final weeks = (difference.inDays / 7).floor();
+      return '$weeks주 전';
+    } else if (now.year == year) {
+      return '$month월 $day일';
+    } else {
+      return '$year년 $month월 $day일';
+    }
   }
 }
